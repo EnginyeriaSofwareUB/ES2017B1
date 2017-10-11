908884b209a87edbe9f6132f5044a845a3dbd4ff
catiza2003 <catiza2003@hotmail.com>
2017-10-11 19:45:52 +0200

    Implementacion de Mapa del Juego

---
+++ Assets/Sly hunting/scripts/Jugador.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour {
+    public float Velocidad;
+    public Rigidbody rb;
+    // Use this for initialization
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        float MoverHorizontal = Input.GetAxis("Horizontal");
+        float MoverVertical = Input.GetAxis("Vertical");
+
+        Vector3 movimiento = new Vector3(MoverHorizontal,0.0f,MoverVertical);
+        
+        rb.AddForce(movimiento * Velocidad * Time.deltaTime);
+        
+    }
+}
