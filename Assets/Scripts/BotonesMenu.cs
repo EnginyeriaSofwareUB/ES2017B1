90f8d6a9e950ffc2184db8171037d47651bd4fc1
RokaZetsu <rokanas666@gmail.com>
2017-10-11 21:22:33 +0200

    #6
    Script de menu

---
+++ Assets/Scripts/BotonesMenu.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class BotonesMenu : MonoBehaviour {
+    public bool Jugar = false;
+    public bool Opciones = false;
+    public bool Salir = false;
+    // Use this for initialization
+    void Start () {
+        Cursor.visible = true;
+        
+		
+	}
+
+    // Update is called once per frame
+    public void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+        
+    }
+    public void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+
+    }
+    public void OnMouseUpAsButton()
+    {
+        if (Jugar)
+            SceneManager.LoadScene("Numero de la escene");
+        if (Opciones)
+            SceneManager.LoadScene("Numero de la escene");
+        if (Salir)
+            Application.Quit();
+    }
+}
