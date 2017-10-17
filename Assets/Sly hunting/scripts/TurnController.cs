daab5e5e6d458164c9c1fa2be2dafebf5862698d
Jromaa <jr.acosta.ch@gmail.com>
2017-10-18 01:16:42 +0200

    Modificació del sistema de torns

--- Assets/Sly hunting/scripts/TurnController.cs
+++ Assets/Sly hunting/scripts/TurnController.cs
@@ -7,28 +7,34 @@ public class TurnController : MonoBehaviour {
 	public bool pl1;
 	public bool pl2;
 	public float timer = 0.0f;
-	public float startTime = 30.0f;
-	public bool estatTimer = true;
-	public Button button;
-	private Text txt;
+	public float startTime = 10.0f;
+	public bool estatTimer;
+	//public Button button;
+	public Text txt;
+	public Text txt2;
 
 	// Use this for initialization
-	void init () {
-		Button btn = button.GetComponent<Button> ();
-		btn.onClick.AddListener (buttonReset);
+	void Awake () {
+		//Button btn = button.GetComponent<Button> ();
+		//btn.onClick.AddListener (buttonReset);
 		pl1 = true;
 		pl2 = false;
-		timer = startTime;	
+		timer = startTime;
+		estatTimer = true;
+		setText ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 		if (estatTimer) {
-			displayTimer ();
+			//displayTimer ();
 			timer -= Time.deltaTime;
 
 			if (timer <= 0) {
-				changeTurn ();	
+				
+				changeTurn ();
+				setText ();
 			}
 		}
 	}
@@ -41,20 +47,35 @@ public class TurnController : MonoBehaviour {
 		pl1 = !pl1;
 		pl2 = !pl2;
 		timer = startTime;
+
 	}
 
-	void buttonReset(){
-		changeTurn ();
+	void buttonStop(){
+		stop ();
 	}
 
-	void displayTimer(){
+	void setText()
+	{
+		if (pl1) {
+			txt2.text = "";
+			txt.text = "Turn del jugador 1";
+
+		}
+		else{
+			txt.text = "";
+			txt2.text = "Turn del jugador 2";
+
+		}
+	}
+
+	/*void displayTimer(){
 		int displaySeconds;
 		displaySeconds = (int) timer % 60;
 		string text = string.Format ("{1:00}", displaySeconds);
 		txt.text = text;
 
 
-	}
+	}*/
 
 		
 }
