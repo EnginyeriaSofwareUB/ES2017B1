d08f2fea4101b885f5bc3969c502a0bfee154eeb
staskbieto <stask.bieto@gmail.com>
2017-10-25 20:36:28 +0200

    Millorant el moviment... Encara incorrecte

--- Assets/Sly hunting/scripts/Jugador.cs
+++ Assets/Sly hunting/scripts/Jugador.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -7,16 +7,17 @@ using System;
 public class Jugador : MonoBehaviour {
 
 	public Rigidbody2D rb;
-	public float speed = 1.0f;
+	public float speed = 10.0f;
 	public float FPS = 60f;
 	private SpriteRenderer mySpriteRenderer;
 	public bool toRight = true;
-
 	public bool playerControl;
+	private bool jumping = false;
 
 	private void Start()
 	{
 		rb = GetComponent<Rigidbody2D> ();
+		rb.mass = 500f;
 		mySpriteRenderer = GetComponent<SpriteRenderer> ();
 	}
 	void Awake () {
@@ -34,7 +35,9 @@ public class Jugador : MonoBehaviour {
 					mySpriteRenderer.flipX = true;
 					toRight = false;
 				}
-				rb.transform.Translate (Vector3.left * speed * Time.deltaTime * FPS);
+				float move = Input.GetAxis ("Horizontal");
+				rb.velocity = new Vector2 ((-1)*speed * Time.deltaTime * FPS, rb.velocity.y);
+				//rb.transform.Translate (Vector3.left * speed * Time.deltaTime * FPS);
 			}
 		}
 
@@ -44,13 +47,26 @@ public class Jugador : MonoBehaviour {
 					mySpriteRenderer.flipX = false;
 					toRight = true;
 				}
-				rb.transform.Translate (Vector3.right * speed * Time.deltaTime * FPS);
+				float move = Input.GetAxis ("Horizontal");
+				rb.velocity = new Vector2 (speed * Time.deltaTime * FPS, rb.velocity.y);
+				//rb.transform.Translate (Vector3.right * speed * Time.deltaTime * FPS);
 			} 
 		}
-		if (Input.GetKey (KeyCode.Space)) {
-			if (playerControl)
-				rb.transform.Translate (Vector3.up * speed * Time.deltaTime * FPS);
+		if (Input.GetKey (KeyCode.Space) && rb.velocity.y <= 0) {
 			
+			if (playerControl && !jumping) {
+				jumping = true;
+				float startGravity = rb.gravityScale;
+				rb.gravityScale = 0;
+				rb.velocity = new Vector2 (rb.velocity.x, speed * Time.deltaTime * FPS);
+				float timer = 0f;
+				while (timer < 2f) {
+					timer += Time.deltaTime;
+				}
+				//Set gravity back to normal at the end of the jump
+				rb.gravityScale = startGravity;
+				jumping = false;
+			}
 		}
 			
 	}
