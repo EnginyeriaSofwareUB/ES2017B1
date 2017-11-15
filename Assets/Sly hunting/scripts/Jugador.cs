d5305ae28a0808cf919c8bf61273c8199a40d3c8
staskbieto <stask.bieto@gmail.com>
2017-11-15 17:54:44 +0100

    Camera fix i estamina

--- Assets/Sly hunting/scripts/Jugador.cs
+++ Assets/Sly hunting/scripts/Jugador.cs
@@ -13,6 +13,8 @@ public class Jugador : MonoBehaviour {
 	public bool toRight = true;
 	public bool playerControl;
 	private bool jumping = false;
+	private float estamina = 100;
+	private float vida = 100;
 
 	private void Start()
 	{
@@ -25,52 +27,67 @@ public class Jugador : MonoBehaviour {
 		playerControl = false;
 	}
 
+	public float getEstamina(){
+		return estamina;
+	}
+
+	public void addEstamina(float val){
+		estamina += val;
+	}
+
+	public void subEstamina(float val){
+		estamina -= val;
+	}
+
 	public void setPlayerControl (bool what){
 		playerControl = what;
 	}
+
+
     // Update is called once per frame
     void Update () {
-		if (Input.GetKey (KeyCode.LeftArrow)) {
-			if (playerControl) {
-				if (toRight) {
-					Vector3 newScale = this.transform.localScale;
-					newScale.x *= -1;
-					this.transform.localScale = newScale;
-					toRight = false;
+		float stam = estamina - (estamina/5); //cada acción resta un 1/5 = 20%
+		if (playerControl && stam > 0) {
+			if (Input.GetKey (KeyCode.LeftArrow) ) {
+				if (playerControl) {
+					if (toRight) {
+						Vector3 newScale = this.transform.localScale;
+						newScale.x *= -1;
+						this.transform.localScale = newScale;
+						toRight = false;
+					}
+					rb.velocity = new Vector2 ((-1) * speed * Time.deltaTime * FPS, rb.velocity.y);
 				}
-				rb.velocity = new Vector2 ((-1)*speed * Time.deltaTime * FPS, rb.velocity.y);
-				//rb.transform.Translate (Vector3.left * speed * Time.deltaTime * FPS);
 			}
-		}
 
-		if (Input.GetKey (KeyCode.RightArrow)) {
-			if (playerControl) {
-				if (!toRight) {
-					Vector3 newScale = this.transform.localScale;
-					newScale.x *= -1;
-					this.transform.localScale = newScale;					
-					toRight = true;
-				}
-				rb.velocity = new Vector2 (speed * Time.deltaTime * FPS, rb.velocity.y);
-				//rb.transform.Translate (Vector3.right * speed * Time.deltaTime * FPS);
-			} 
-		}
-		if (Input.GetKey (KeyCode.Space) && rb.velocity.y <= 0) {
-			
-			if (playerControl && !jumping) {
-				jumping = true;
-				float startGravity = rb.gravityScale;
-				rb.gravityScale = 0;
-				rb.velocity = new Vector2 (rb.velocity.x, speed * Time.deltaTime * FPS);
-				float timer = 0f;
-				while (timer < 2f) {
-					timer += Time.deltaTime;
-				}
-				//Set gravity back to normal at the end of the jump
-				rb.gravityScale = startGravity;
-				jumping = false;
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				if (playerControl) {
+					if (!toRight) {
+						Vector3 newScale = this.transform.localScale;
+						newScale.x *= -1;
+						this.transform.localScale = newScale;					
+						toRight = true;
+					}
+					rb.velocity = new Vector2 (speed * Time.deltaTime * FPS, rb.velocity.y);
+				} 
 			}
-		}
+			if (Input.GetKey (KeyCode.Space) && rb.velocity.y <= 0) {
 			
+				if (playerControl && !jumping) {
+					jumping = true;
+					float startGravity = rb.gravityScale;
+					rb.gravityScale = 0;
+					rb.velocity = new Vector2 (rb.velocity.x, speed * Time.deltaTime * FPS);
+					float timer = 0f;
+					while (timer < 2f) {
+						timer += Time.deltaTime;
+					}
+					//Set gravity back to normal at the end of the jump
+					rb.gravityScale = startGravity;
+					jumping = false;
+				}
+			}
+			
+		}
 	}
 }
