fb4d67c55bce31b679b31a7aac40150037ec310e
Jromaa <jr.acosta.ch@gmail.com>
2017-11-15 01:36:59 +0100

    #30 Implementació OpcionsMenu

---
+++ Assets/Sly hunting/scripts/ControladorOpcions.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ControladorOpcions : MonoBehaviour {
+
+	public void MenuScene() {
+		SceneManager.LoadScene ("MenuPrincipal");
+
+	}
+}
+
