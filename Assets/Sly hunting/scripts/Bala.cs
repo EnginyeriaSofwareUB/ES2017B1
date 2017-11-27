68e63889dc6abf35c733fddfb3c5efce8b821981
MichaelShakke <sector_mich@hotmail.com>
2017-11-27 04:00:21 +0100

    Accion disparar hecha

---
+++ Assets/Sly hunting/scripts/Bala.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+using System;
+
+public class Bala : MonoBehaviour {
+	public float speed;
+	private Rigidbody2D rb;
+	private Jugador jugador;
+
+	void Start() {
+		rb = GetComponent<Rigidbody2D> ();
+		Debug.Log (transform.eulerAngles.z);
+
+		foreach (Jugador j in FindObjectsOfType<Jugador> ()) {
+			if (j.playerControl) {
+				jugador = j;
+				break;
+			}
+		}
+		float inclinacion = 0f;
+		if (transform.eulerAngles.z <= 90.0f) {
+			inclinacion = transform.eulerAngles.z / 91;
+		} else if (transform.eulerAngles.z >= 260.0f && transform.eulerAngles.z <= 360.0f) {
+			inclinacion = ((transform.eulerAngles.z - 260) / 110) - 1;
+		}
+		Debug.Log (inclinacion);
+		if (jugador.izquierda) 
+				rb.AddForce (new Vector2 (-1, inclinacion) * speed, ForceMode2D.Impulse);
+			else rb.AddForce (new Vector2 (1, inclinacion) * speed, ForceMode2D.Impulse);
+	}
+
+	void Update() {
+//		rb.velocity = new Vector2 (speed, rb.velocity.y);
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		Destroy (gameObject);
+	}
+
+	void OnCollisionEnter2D(Collider2D other) {
+		Destroy (gameObject);
+	}
+}
--- Assets/Sly hunting/scripts/Jugador.cs
+++ Assets/Sly hunting/scripts/Jugador.cs
@@ -15,14 +15,23 @@ public class Jugador : MonoBehaviour {
 	private float estamina = 100;
 	private float vida = 100;
 	private Animator animator;
-    bool izquierda = true;
-    bool derecha = false;
+	public bool izquierda;
+	public bool derecha;
+	private List<Arma> weapons;
+	int currentWeapon;
+
+	public Transform puntoFuego;
+	float fireRate = 0.5f;
+	float nextFire = 0f;
 
 
 	float forceJump = 20.0f;
 
 	private void Start()
 	{
+		currentWeapon = 0;
+		izquierda = true;
+		derecha = false;
 		rb = GetComponent<Rigidbody2D> ();
 		animator = GetComponent<Animator>();
 
@@ -32,67 +41,63 @@ public class Jugador : MonoBehaviour {
 	}
 	void Awake () {
 		playerControl = false;
-        //animator.StopPlayback();
-        //idle();
-    }
-
-	public float getEstamina(){
-		return estamina;
+		//animator.StopPlayback();
+		//idle();
 	}
 
-	public void addEstamina(float val){
-		estamina += val;
-	}
-
-	public void subEstamina(float val){
-		estamina -= val;
-	}
-
-	public void setPlayerControl (bool what){
-		playerControl = what;
-	}
-
-    // Update is called once per frame
-    void Update () {
+	// Update is called once per frame
+	void Update () {
 
 		float stam = estamina - (estamina/5); //cada acción resta un 1/5 = 20%
 		if (playerControl && stam > 0) {
 			if (Input.GetKey (KeyCode.LeftArrow)) {
 				moveLeft ();
-                
+
 			} else if (Input.GetKey (KeyCode.RightArrow)) {
 				moveRight ();
-                
 
 			} else if (Input.GetKey (KeyCode.Space) && rb.velocity.y <= 0) {
 				jump ();
-			
+
+			} else if (Input.GetKey(KeyCode.UpArrow)) {
+				lookUp();
+
+			} else if (Input.GetKey(KeyCode.DownArrow)) {
+				lookDown();
+
+			} else if (Input.GetKey (KeyCode.Return)) {
+				fire ();
+
 			} else {
 				idle ();
 			}
-			
+
 		}
 	}
 
-	void moveRight(){
-        /*
+	private void moveRight(){
+		/*
         if (toRight) {
 			Vector3 newScale = this.transform.localScale;
 			newScale.x *= -1;
 			this.transform.localScale = newScale;
 			toRight = false;
 		}*/
-        animator.StopPlayback();
-        animator.Play("rightRun");
-        //rb.velocity = new Vector2( speed * Time.deltaTime * FPS, rb.velocity.y);
-        this.transform.Translate(Vector3.right * speed * Time.deltaTime * FPS);
-        derecha = true;
-        izquierda = false;
+		animator.StopPlayback();
+		animator.Play("rightRun");
+		//rb.velocity = new Vector2( speed * Time.deltaTime * FPS, rb.velocity.y);
+		Quaternion aux = this.transform.rotation;
+		aux.y = 0;
+		this.transform.rotation = aux;
+		this.transform.Translate(Vector3.right * speed * Time.deltaTime * FPS);
 
-    }
+		derecha = true;
+		izquierda = false;
 
-	void moveLeft(){
-        /*
+	}
+
+	private void moveLeft(){
+		/*
 		if (!toRight) {
 			Vector3 newScale = this.transform.localScale;
 			newScale.x *= -1;
@@ -100,16 +105,19 @@ public class Jugador : MonoBehaviour {
 			toRight = true;
 		}*/
 		animator.StopPlayback();		
-		animator.Play("leftRun");
-        //rb.velocity = new Vector2(speed * Time.deltaTime * FPS, rb.velocity.y);
-        //rb.velocity = new Vector2(-1 * speed * Time.deltaTime * FPS, rb.velocity.y);
-        this.transform.Translate(Vector3.right * (-1) * speed * Time.deltaTime * FPS);
-        izquierda = true;
-        derecha = false;
-    }
+		animator.Play("rightRun");
+		//rb.velocity = new Vector2(speed * Time.deltaTime * FPS, rb.velocity.y);
+		//rb.velocity = new Vector2(-1 * speed * Time.deltaTime * FPS, rb.velocity.y);
+		Quaternion aux = this.transform.rotation;
+		aux.y = 180;
+		this.transform.rotation = aux;
+		this.transform.Translate(Vector3.right * speed * Time.deltaTime * FPS);
+		izquierda = true;
+		derecha = false;
+	}
 
 
-	void jump(){
+	private void jump(){
 		if (!jumping) {
 			jumping = true;
 			animator.StopPlayback();
@@ -129,9 +137,79 @@ public class Jugador : MonoBehaviour {
 		}
 
 	}
-	void idle(){
+
+	private void lookUp() {
+		float angle  = puntoFuego.transform.localEulerAngles.z + 1;
+		Debug.Log (angle);
+		if ((angle  <= 90.0f && angle >= 0.0f) || (angle <= 361.0f && angle >= 270.0f)) {
+			puntoFuego.transform.Rotate (0, 0, 1);	
+		}
+	}
+
+	private void lookDown() {
+		float angle = puntoFuego.transform.localEulerAngles.z;
+		Debug.Log (angle);
+		if ((angle  <= 90.0f && angle >= 0.0f) || (angle <= 361.0f && angle >= 270.0f)) {
+			puntoFuego.transform.Rotate (0, 0, -1);	
+		}
+
+	}
+
+	private void fire() {	
+		Arma currentW = weapons [currentWeapon];
+		if (currentW.getBullets () > 0) {
+			if (Time.time > nextFire) {
+				nextFire = Time.time + fireRate;
+				if (derecha) {
+					Instantiate (currentW.getPrefab(), puntoFuego.position, puntoFuego.transform.rotation);
+				} else if (izquierda) {
+					Instantiate (currentW.getPrefab(), puntoFuego.position, puntoFuego.transform.rotation);
+				}
+				currentW.decreaseBullet ();
+			}
+		}//else no bullets
+
+
+	}
+	private void idle(){
 		animator.StopPlayback();
-		if (derecha) animator.Play("rightIdle");
-        else animator.Play("leftIdle");
-    }
+		animator.Play("rightIdle");
+	}
+
+	//****************** GETTERS & SETTERS **********************/
+
+	public void setWeapons(List<Arma> armas) {
+		weapons = armas;
+	}
+
+	private void addWeapon(Arma weapon) {
+
+	}
+	//We call this method when we find a box with a new weapon and we have already added to the list
+	public void updateWeaponsTeam(Arma weapon) {
+		ControlladorPartida cp = GetComponent<ControlladorPartida> ();
+		cp.getCurrentTeam ().addWeapon (weapons, weapon);
+	}
+
+	//update weapons of team
+	public void updateWeaponsTeam() {
+		ControlladorPartida cp = (ControlladorPartida) FindObjectOfType (typeof(ControlladorPartida));
+		cp.getCurrentTeam ().updateWeapons(weapons);
+	}
+
+	public float getEstamina(){
+		return estamina;
+	}
+
+	public void addEstamina(float val){
+		estamina += val;
+	}
+
+	public void subEstamina(float val){
+		estamina -= val;
+	}
+
+	public void setPlayerControl (bool what){
+		playerControl = what;
+	}
 }
