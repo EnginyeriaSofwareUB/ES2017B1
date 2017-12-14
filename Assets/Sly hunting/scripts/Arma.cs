270155fd53f63dea0c2d55c81c27ff509bdd158b
staskbieto <stask.bieto@gmail.com>
2017-12-14 18:18:01 +0100

    Arma Bazoca
    - Falta hacer el visual de las armas
    - Se cambia de arma con la letra C
    - FIX: las cajas no se 'cojen' si no eres el actual player.
    - Más recompensas cajas buenas

--- Assets/Sly hunting/scripts/Arma.cs
+++ Assets/Sly hunting/scripts/Arma.cs
@@ -11,19 +11,27 @@ public class Arma  {
 	float nextFire = 0f;
 	float fireRate = 0.5f;
 	float damage = 20.0f; 
+	String type;
 
-
-	public Arma(int bullets)  {
+	public Arma(int bullets,String type="regular")  {
 		this.bullets = bullets;
+		this.type = type;
+		if (type == "Bazoca") {
+			damage *= 2f; //incrementem un 50% si es Bazoca
+		}
+	}
+
+	public String getTipus(){
+		return type;
 	}
 
 	public void fire(Boolean toRight,Transform puntoFuego,Jugador actual){
-		if (Time.time > nextFire) {
+		if (Time.time > nextFire && getBullets() > 0) {
 			nextFire = Time.time + fireRate;
 			if (actual.isDestructor ()) {
-				Bala.create (puntoFuego, damage, actual);
+				Bala.create (puntoFuego, damage*1.5f, actual);//incrementem un 50% si dispara un destructor
 			} else {
-				Bala.create (puntoFuego, damage*1.5f, actual);
+				Bala.create (puntoFuego, damage, actual);
 			}
 			decreaseBullet ();
 		}
