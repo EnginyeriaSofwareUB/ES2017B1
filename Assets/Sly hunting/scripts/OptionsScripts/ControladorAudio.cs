c50f0bfb4d7a0c71529f28ad9bd81b322c9613dd
Jromaa <jr.acosta.ch@gmail.com>
2017-12-16 12:06:38 +0100

    #124 Sonido inicial, bug de cambio de escena, modificacion de suelo

--- Assets/Sly hunting/scripts/OptionsScripts/ControladorAudio.cs
+++ Assets/Sly hunting/scripts/OptionsScripts/ControladorAudio.cs
@@ -5,9 +5,12 @@ using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class ControladorAudio : MonoBehaviour {
+	
 	GameObject[] objs;
+
 	void Awake(){
 		objs = GameObject.FindGameObjectsWithTag ("Music");
+		this.gameObject.GetComponent<AudioSource> ().volume = 0.1f;
 		if (objs.Length > 1)
 			Destroy (this.gameObject);
 		DontDestroyOnLoad (this.gameObject);
