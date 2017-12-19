fde849166dcc4f8291a086a6d1a0915728810090
Jromaa <jr.acosta.ch@gmail.com>
2017-12-20 00:11:31 +0100

    #89 Boton cambiar turno

--- Assets/Sly hunting/scripts/BotonesSeleccionEquipo.cs
+++ Assets/Sly hunting/scripts/BotonesSeleccionEquipo.cs
@@ -8,12 +8,27 @@ public class BotonesSeleccionEquipo : MonoBehaviour {
 
 	GameObject obj;
 	GameObject slider;
+	Image monoI;
+	Image cazadorD;
+	Image monoD;
+	Image cazadorI;
 	Text txt;
 
 	void Awake() {
-		slider = GameObject.Find ("numPlayers");
+		
 		txt = GameObject.Find ("txtNumPlayers").GetComponent<Text> ();
 		txt.text = "3 Jugadores";
+
+		monoI = GameObject.Find ("MonoI").GetComponent<Image> ();
+		cazadorD = GameObject.Find ("CazadorD").GetComponent<Image> ();
+		monoD = GameObject.Find ("MonoD").GetComponent<Image> ();
+		cazadorI = GameObject.Find ("CazadorI").GetComponent<Image> ();
+		monoI.enabled = true;
+		monoD.enabled = false;
+		cazadorI.enabled = false;
+		cazadorD.enabled = true;
+
+		slider = GameObject.Find ("numPlayers");
 		slider.GetComponent<Slider> ().minValue = 5;
 		slider.GetComponent<Slider> ().maxValue = 15;
 
@@ -31,11 +46,17 @@ public class BotonesSeleccionEquipo : MonoBehaviour {
 		SceneManager.LoadScene ("scenary");
 
 	}
-
-
-	public void ChangePlayer() {
+		
+	public void changeNumPlayers() {
 		txt.text = ((int)slider.GetComponent<Slider> ().value).ToString ();
 		txt.text += " Jugadores";
 		PlayerPrefs.SetInt ("nPlayers", (int)slider.GetComponent<Slider> ().value);
+	}
+
+	public void changePlayer() {
+		monoI.enabled = !monoI.enabled;
+		monoD.enabled = !monoD.enabled;
+		cazadorI.enabled = !cazadorI.enabled;
+		cazadorD.enabled = !cazadorD.enabled;
 	}
 }
