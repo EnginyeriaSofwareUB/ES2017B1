af19ea99aaf25160f9ad9820da7ff5292e79dce4
Jromaa <jr.acosta.ch@gmail.com>
2017-12-19 23:00:24 +0100

    #89 Numero de jugadores para empezar

--- Assets/Sly hunting/scripts/BotonesSeleccionEquipo.cs
+++ Assets/Sly hunting/scripts/BotonesSeleccionEquipo.cs
@@ -2,12 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class BotonesSeleccionEquipo : MonoBehaviour {
 
 	GameObject obj;
+	GameObject slider;
+	Text txt;
 
 	void Awake() {
+		slider = GameObject.Find ("numPlayers");
+		txt = GameObject.Find ("txtNumPlayers").GetComponent<Text> ();
+		txt.text = "3 Jugadores";
+		slider.GetComponent<Slider> ().minValue = 5;
+		slider.GetComponent<Slider> ().maxValue = 15;
+
 		if (GameObject.FindGameObjectWithTag ("controlOptions") != null) {
 			obj = GameObject.FindGameObjectWithTag ("controlOptions");
 			Destroy (obj);
@@ -22,11 +31,11 @@ public class BotonesSeleccionEquipo : MonoBehaviour {
 		SceneManager.LoadScene ("scenary");
 
 	}
-	//public void ChangePlayer1(){
-		//GameObject.Find("PlayerJ1").GetComponent<Button>().image.overrideSprite = "completeMonkey.psd";
 
 
-
-
-	//}
+	public void ChangePlayer() {
+		txt.text = ((int)slider.GetComponent<Slider> ().value).ToString ();
+		txt.text += " Jugadores";
+		PlayerPrefs.SetInt ("nPlayers", (int)slider.GetComponent<Slider> ().value);
+	}
 }
