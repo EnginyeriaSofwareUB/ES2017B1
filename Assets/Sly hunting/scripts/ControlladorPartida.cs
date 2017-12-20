fa7b4bc1620b1eb7010628a01a61476a8f9cf878
Jromaa <jr.acosta.ch@gmail.com>
2017-12-20 21:12:05 +0100

    #89 Escena seleccion personajes

--- Assets/Sly hunting/scripts/ControlladorPartida.cs
+++ Assets/Sly hunting/scripts/ControlladorPartida.cs
@@ -54,7 +54,7 @@ public class ControlladorPartida : MonoBehaviour
 
 	public  GameObject actual;
 	private Camera camera;
-	private int numero_jugadores = 3; //son 3 
+	private int numero_jugadores; //son 3 
 	private Equipo actualEquipo;
 
 	// Tiempo caida caja
@@ -77,7 +77,7 @@ public class ControlladorPartida : MonoBehaviour
 
 	void Awake ()
 	{
-
+		numero_jugadores = PlayerPrefs.GetInt ("nPlayers");
 		hunters = new Equipo ("Cazador",numero_jugadores,this);
 		animals = new Equipo ("Mono",numero_jugadores,this);
 
